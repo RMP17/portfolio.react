bcc1d3cad5757527aad27e4cb14f7a439327c52b
Rider Mamani <rdrmamani@gmail.com>
2021-03-15 19:07:51 -0400

    docs actualizado

---
+++ docs/precache-manifest.dea38ca84ebfa48617f2404bc91845f2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e04b75549407f7f4654524d5bacdc9",
+    "url": "/portfolio.react/index.html"
+  },
+  {
+    "revision": "d054243a7f89f06f092c",
+    "url": "/portfolio.react/static/css/3.19a8803c.chunk.css"
+  },
+  {
+    "revision": "8b2a8db2d644146d638f",
+    "url": "/portfolio.react/static/css/main.a487ba7d.chunk.css"
+  },
+  {
+    "revision": "31d34104006e63094d46",
+    "url": "/portfolio.react/static/js/2.52ee159f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio.react/static/js/2.52ee159f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d054243a7f89f06f092c",
+    "url": "/portfolio.react/static/js/3.07b8e2e5.chunk.js"
+  },
+  {
+    "revision": "8b2a8db2d644146d638f",
+    "url": "/portfolio.react/static/js/main.cab4acf4.chunk.js"
+  },
+  {
+    "revision": "569283166eedbf898247",
+    "url": "/portfolio.react/static/js/runtime-main.bb4af1c9.js"
+  },
+  {
+    "revision": "e664a68a4575292848410f3161fbcf2a",
+    "url": "/portfolio.react/static/media/documentCV.e664a68a.pdf"
+  },
+  {
+    "revision": "83f5907bc59e1ed961f844daf660498b",
+    "url": "/portfolio.react/static/media/img1.83f5907b.jpg"
+  },
+  {
+    "revision": "2b84693c3df789778908a086e90759a2",
+    "url": "/portfolio.react/static/media/photo.2b84693c.png"
+  }
+]);
