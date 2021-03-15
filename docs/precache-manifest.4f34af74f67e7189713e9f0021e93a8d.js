bc1e81910a4a8b13ab955b8f2390c22e2b0a79ef
Rider Mamani <rdrmamani@gmail.com>
2021-03-15 18:26:51 -0400

    docs agregado

---
+++ docs/precache-manifest.4f34af74f67e7189713e9f0021e93a8d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35fa943e2aade588d4099a546ca8ca86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d054243a7f89f06f092c",
+    "url": "/static/css/3.19a8803c.chunk.css"
+  },
+  {
+    "revision": "5cb887ae8be50c698fd5",
+    "url": "/static/css/main.4ba5186f.chunk.css"
+  },
+  {
+    "revision": "31d34104006e63094d46",
+    "url": "/static/js/2.52ee159f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.52ee159f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d054243a7f89f06f092c",
+    "url": "/static/js/3.07b8e2e5.chunk.js"
+  },
+  {
+    "revision": "5cb887ae8be50c698fd5",
+    "url": "/static/js/main.e6e059f8.chunk.js"
+  },
+  {
+    "revision": "92a4b3d56e803a79b3a3",
+    "url": "/static/js/runtime-main.c051915c.js"
+  },
+  {
+    "revision": "e664a68a4575292848410f3161fbcf2a",
+    "url": "/static/media/documentCV.e664a68a.pdf"
+  },
+  {
+    "revision": "83f5907bc59e1ed961f844daf660498b",
+    "url": "/static/media/img1.83f5907b.jpg"
+  },
+  {
+    "revision": "2b84693c3df789778908a086e90759a2",
+    "url": "/static/media/photo.2b84693c.png"
+  }
+]);
